cf1860a45b33f4505822d9b0b02467f7bf3f3b0d
lzc <lzc18910847603@163.com>
2022-03-12 10:31:35 +0800

    第二周作业6

---
+++ homework/src/main/java/com/geek/homework/HttpClientUtils.java
@@ -0,0 +1,35 @@
+package com.geek.homework;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientUtils {
+
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    public static String getAsString(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            return EntityUtils.toString(entity1, "UTF-8");
+        } finally {
+            response1.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "http://localhost:8801";
+        String text = HttpClientUtils.getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+
+    }
+}
---
+++ homework/src/main/java/com/geek/homework/OkHttpUtils.java
@@ -0,0 +1,31 @@
+package com.geek.homework;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpUtils {
+    public static OkHttpClient client = new OkHttpClient();
+
+    public static String getAsString(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "http://localhost:8801";
+        String text = OkHttpUtils.getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+
+        // 清理资源
+        OkHttpUtils.client = null;
+    }
+}
